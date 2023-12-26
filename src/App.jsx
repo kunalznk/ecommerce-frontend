93ecef72f5d98d41f45f0a7aa411a1132b8c806d
Akshayw26 <wangade30@gmail.com>
2023-12-26 13:13:34 +0530

    Login Page Developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
       <div className="flex flex-col justify-center items-center w-full h-dvh">
         <Home />
+        <LoginPage />
       </div>
     </>
   );
