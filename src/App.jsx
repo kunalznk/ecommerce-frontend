32fd27b83bbeb9ba63fbebd86c01f52ae9fc7695
Akshayw26 <wangade30@gmail.com>
2023-12-27 12:44:34 +0530

    Login and Register and Edit Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
   return (
     <>
       <div className="flex flex-col justify-center items-center w-full h-dvh">
-        <Home />
-        <LoginPage />
+        {/* <Home /> */}
+        {/* <LoginPage /> */}
+        {/* <RegisterPage/> */}
+         <EditProfile/>
       </div>
     </>
   );
