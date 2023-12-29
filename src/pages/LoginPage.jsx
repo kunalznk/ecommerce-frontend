3efac3d17e29b6e4c73a56ce5f1ac328249ccc76
Akshayw26 <wangade30@gmail.com>
2023-12-29 20:54:15 +0530

    Contact Billing HomePage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import { Typography, Button } from "@material-tailwind/react";
-import { Input } from "@material-tailwind/react";
-
-const LoginPage = () => {
-  return (
-    <div className="w-4/5 flex h-4/5">
-      <div className="w-full h-full hidden sm:block">
-        <img
-          className="h-full w-full object-cover object-center"
-          src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-          alt="nature image"
-        />
-      </div>
-      <div className="w-full flex justify-end items-center">
-        <div className="flex flex-col gap-10 justify-center">
-          <div className="flex flex-col gap-6">
-            <Typography className="font-medium text-4xl">
-              Log in Into Exclusive
-            </Typography>
-            <Typography className="font-normal text-base">
-              Enter your details below
-            </Typography>
-          </div>
-          <div className="flex flex-col gap-10">
-            <Input
-              variant="standard"
-              label="Email or Phone Number"
-              placeholder="Email or Phone Number"
-            />
-            <Input
-              variant="standard"
-              label="Password"
-              placeholder="Passwordr"
-              type="password"
-            />
-          </div>
-          <Button variant="gradient" size="sm">
-            {" "}
-            Login{" "}
-          </Button>
-          <Button variant="text" size="sm">
-            Forget Password?
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-=======
 import { Typography, Button, Alert } from "@material-tailwind/react";
 import { Input } from "@material-tailwind/react";
 import axios from "axios";
@@ -216,5 +165,4 @@ const LoginPage = () => {
   );
 };
 
->>>>>>> 3ac6b0c8dbe7fa46010553002b3bd98683a65beb
 export default LoginPage;
